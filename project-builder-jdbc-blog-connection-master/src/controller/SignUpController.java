167e31e0aceecb52a171c9a4b6cead7b6f127207
Naga Sai Chaitanya <55658505+naga-sai-chaitanya@users.noreply.github.com>
2020-07-30 22:34:05 +0530

    Add files via upload

---
+++ project-builder-jdbc-blog-connection-master/src/controller/SignUpController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utility.ConnectionManager;
+
+
+
+@WebServlet(urlPatterns= {"/signup"})
+public class SignUpController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public SignUpController() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Sign up Controller");
+		ConnectionManager cm = new ConnectionManager();
+		cm.getConnection();
+		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
+		rd.forward(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request,response);
+		
+	}
+
+}
